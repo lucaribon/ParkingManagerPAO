a654dd7b7c59b3fbc26f33de06b6e851704a9490
lucaribon <lucaribon0203@gmail.com>
2024-05-06 13:45:51 +0200

    added app icon

--- ParkingManager/main.cpp
+++ ParkingManager/main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QIcon appIcon(":/assets/icons/parking-app-icon.svg");
+    a.setWindowIcon(appIcon);
     MainWindow w;
     w.show();
     return a.exec();
